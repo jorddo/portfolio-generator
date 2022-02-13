7eca651b8f878c72b7da68ec937427a0117d5e74
jorddo <jordanbsneed@gmail.com>
2022-02-13 15:15:40 -0600

    generating html file - fixes#2

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// file system module
+const fs = require('fs');
+// page-template.js link
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const generatePage = () => 'Name: Jane, Github: janehub';
+const [name, github] = profileDataArgs; // basically the same as the next two lines
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+
+fs.writeFile('./index.html', generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see output!');
+});
